058af3bd337c0a74c99a3a733dd5dbc41a5c57bd
wholem1lk <wwscottbc@gmail.com>
2020-10-31 19:25:39 -0700

    cleaned code and transfered main timer to function.

--- main.py
+++ main.py
@@ -5,29 +5,36 @@
 import time
 import sys
 
+
+def PomTimer(duration=1500):
+    global pomCounter
+    pomDuration = duration
+    while pomDuration:
+        min, sec = divmod(pomDuration, 60)
+        timer = f'{min:02d}:{sec:02d}'
+        print(timer, end='\r')
+        time.sleep(1)
+        pomDuration -= 1
+    pomCounter += 1
+    print(f"Done!!! You have completed {pomCounter} Pomodoros!")
+
+
 # first, create a simple timer loop (with input)
 pomCounter = 0
 while True:
     print("Current Pom: " + str(pomCounter+1))
     print("Poms until long break: " + str(5-pomCounter))
     answer = input("Hit 'y' to start your next pomodoro. 'n' or 'q' to exit.")
+
     if answer.lower() not in ('y', 'n', 'q'):
         print("Invalid input.")
         sys.exit(1)
     elif answer.lower() == 'y':
-        pomDuration = 10
-        while pomDuration:
-            min, sec = divmod(pomDuration, 60)
-            timer = '{:02d}:{:02d}'.format(min, sec)
-            print(timer, end="\r")
-            time.sleep(1)
-            pomDuration -= 1
-        pomCounter += 1
-        print(f"Times up!!! You have completed {pomCounter} Pomodoros!")
+        PomTimer(10)
         breakDur = 5
         while breakDur:
             min, sec = divmod(breakDur, 60)
-            brTimer = '{:01d}:{:01d}'.format(min, sec)
+            brTimer = '{:02d}:{:02d}'.format(min, sec)
             print(brTimer, end='\r')
             time.sleep(1)
             breakDur -= 1
