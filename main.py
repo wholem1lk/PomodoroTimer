022daa0adee69d63c5b1ee35b4fa571c28acb49c
wholem1lk <wwscottbc@gmail.com>
2020-11-01 20:10:12 -0800

    added new functionality with help function and customizable timer function (in progress.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 
 
 def PomTimer(duration=1500):
+    """Sets Duration of Pomodoro Timer"""
     global pomCounter
     pomDuration = duration
     while pomDuration:
@@ -20,6 +21,7 @@ def PomTimer(duration=1500):
 
 
 def BreakTimer(duration=300):
+    """Sets Duration of Break Timer"""
     breakDur = duration
     while breakDur:
         min, sec = divmod(breakDur, 60)
@@ -30,19 +32,68 @@ def BreakTimer(duration=300):
     print("Your break is over!")
 
 
+def GetDur():
+    """Ask User for Input on Duration of Timers"""
+    print("The")
+    answerOne = input("Do you want to use preset timers? (y/n) \n")
+    if answerOne.lower() not in ('y', 'n'):
+        answerOne.lower()
+        pass
+
+
+def Help():
+    """Provide User with Program Info"""
+    while True:
+        helpAns = input("""
+Choose from the following:
+Enter "1" for info on the Pomodoro Timer.
+Enter "2" for info about the program and author.
+Enter "3" to return to the menu.
+        """)
+        if helpAns not in ('1', '2', '3'):
+            print("Invalid Input.")
+            continue
+        elif helpAns == '1':
+            print("""The Pomodoro Timer is a study/work method to help with focus.
+The intent is to work in 20-25 minute periods, followed by a 3-5
+minute break. The break after the fifth Pomodoro should be longer;
+20 to 25 minutes.
+            """)
+            continue
+        elif helpAns == '2':
+            print("""Hey! If you're reading this - thanks for trying out my program!
+I decided to try my hand at making this program because I wanted a
+Pomodoro Timer - on my computer - that didn't do anything more than
+what it should. That meant no collection of personal info,
+no sending info: just a simple timer with a built in tracker.
+
+Hopefully you've been able to make use of this program in your own
+work and studies.
+
+Cheers,
+Wil
+            """)
+        elif helpAns == '3':
+            break
+
+
 # first, create a simple timer loop (with input)
 pomCounter = 0
 while True:
     print("Current Pom: " + str(pomCounter+1))
     print("Poms until long break: " + str(5-pomCounter))
-    answer = input("Hit 'y' to start your next pomodoro. 'n' or 'q' to exit.")
+    answer = input("""Hit 'y' to start your next pomodoro.
+'n' or 'q' to exit. 'h' for help.
+    """)
 
-    if answer.lower() not in ('y', 'n', 'q'):
+    if answer.lower() not in ('y', 'n', 'q', 'h'):
         print("Invalid input.")
         sys.exit(1)
     elif answer.lower() == 'y':
         PomTimer(10)
         BreakTimer(10)
+    elif answer.lower() == 'h':
+        Help()
     elif answer.lower() in ('q', 'n'):
         print("Goodbye!")
         sys.exit(0)
